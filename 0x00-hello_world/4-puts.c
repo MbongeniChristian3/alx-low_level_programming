2f0fde06960acf5ec5117c26068a5c187616e080
MbongeniChristian3 <moyom8660@gmail.com>
2023-08-10 06:50:39 -0700

    Hello, printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio>
+/**
+ * main - Programming is like building a multilingual puzzle
+ *  Return: 0 (Succes)
+ */
+int main (void)
+{       
+puts("\"Programming is like building a multilingual puzzle");   
+return ()0;
